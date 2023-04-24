bb3cd7d9d3129de361dab82c0d55fdf9bc245e4e
Slava Germonenko <slava.germonenko@gmail.com>
2023-04-24 16:45:56 +0200

    Created Sessions data model.

---
+++ src/Meteor.Sessions.Core/Models/Annotations/IpAddressAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace Meteor.Sessions.Core.Models.Annotations;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class IpAddressAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is not string str)
+        {
+            // Should be handled by Required attribute
+            return true;
+        }
+
+        return IPAddress.TryParse(str, out _);
+    }
+}
---
+++ src/Meteor.Sessions.Core/Models/Session.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Meteor.Sessions.Core.Models.Annotations;
+
+namespace Meteor.Sessions.Core.Models;
+
+public class Session
+{
+    public Guid Id { get; set; }
+
+    public int EmployeeId { get; set; }
+
+    public int CustomerId { get; set; }
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Token is required.")]
+    [StringLength(400, ErrorMessage = "Token max length is 400.")]
+    public string Token { get; set; } = string.Empty;
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Device name is required.")]
+    [StringLength(100, ErrorMessage = "Max device name length is 100.")]
+    public string DeviceName { get; set; } = string.Empty;
+
+    [IpAddress(ErrorMessage = "Must be a valid IP address.")]
+    public string IpAddress { get; set; } = string.Empty;
+
+    public DateTimeOffset CreateDate { get; set; }
+
+    public DateTimeOffset LastRefreshDate { get; set; }
+
+    public DateTimeOffset ExpireDate { get; set; }
+}
